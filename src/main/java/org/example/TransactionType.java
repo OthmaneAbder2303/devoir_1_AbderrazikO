9ac31e60fc501f80c0dbd02ad0a5cf286b27d7b1
Othmane Abderrazik <121362196+OthmaneAbder2303@users.noreply.github.com>
2024-11-07 22:09:06 +0100

    Update TransactionType.java

--- src/main/java/org/example/TransactionType.java
+++ src/main/java/org/example/TransactionType.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public enum TransactionType {
-}
+    VIRINT,  // Transfert interne (même banque)
+    VIREST,  // Transfert externe (différentes banques)
+    VIRMULTA // Transfert multiple (plusieurs comptes dans différentes banques)
+};
