9a116de4038e434bfb2ee77bd6e2b03b24da2068
Othmane Abderrazik <121362196+OthmaneAbder2303@users.noreply.github.com>
2024-11-07 22:11:40 +0100

    Update Banque.java

--- src/main/java/org/example/Banque.java
+++ src/main/java/org/example/Banque.java
@@ -1,4 +1,59 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banque {
+
+    private final String nom;
+    private final String pays;
+    private List<Compte> comptes;
+
+    //Constructor
+    public Banque(String nom, String pays) {
+
+        this.nom = nom;
+        this.pays = pays;
+        this.comptes = new ArrayList<>();
+
+    }
+
+    // Getters and Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public List<Compte> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(List<Compte> comptes) {
+        this.comptes = comptes;
+    }
+
+    //Ajouter un compte à la banque
+    public void AjouterCompte(Compte compte) {
+        comptes.add(compte);
+    }
+
+    //Rechercher un compte dans la banque par n° de compte
+    public Compte RechercherCompte(String numCompte) {
+
+        for(Compte compte : comptes) {
+            if(compte.getNumCompte() == numCompte) {
+                return compte;
+            }
+        }
+        System.out.println("Compte Inexistant");
+        return null;
+
+    }
+
 }
+
+
+
